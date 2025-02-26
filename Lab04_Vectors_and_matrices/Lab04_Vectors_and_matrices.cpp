a0b909327d9f1f55b8c71536762b54b05a0bb1da
Adam Faraj <76076640+AKorty7@users.noreply.github.com>
2025-02-26 14:43:21 +0000

    w

--- Lab04_Vectors_and_matrices/Lab04_Vectors_and_matrices.cpp
+++ Lab04_Vectors_and_matrices/Lab04_Vectors_and_matrices.cpp
@@ -13,5 +13,12 @@ int main() {
     printf("--------------------------------------------------\n");
 
     // Defining vectors
-    glm::vec3
+    glm::vec3 a, b; 
+    a = glm::vec3(3.0f, 0.0f, 4.0f);
+    b[0] = 1.0f, b[1] = 2.0f, b[2] = 3.0f;
+
+    printf("Defining vectors:\n");
+    std::cout << "a = " << a << std::endl;
+    std::cout << "b = " << b << std::endl;
+
 }
